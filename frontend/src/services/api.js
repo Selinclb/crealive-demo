bac84c5d2bedcbcf2b95783dd71eb0557a741569
selin çelebi <selincelebi@mail.com>
2025-02-14 19:15:57 +0300

    demo

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:1337/api';
+const API_URL = process.env.REACT_APP_API_URL || 'https://crealive-demo.onrender.com/api';
 const API_TOKEN = process.env.REACT_APP_STRAPI_API_TOKEN;
 
 const axiosInstance = axios.create({
